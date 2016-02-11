45b58bebafebbe353ffcdc38da5efaa366a470fb
lawrence-iviani <lawrence.iviani@gmail.com>
2016-02-11 16:56:36 +0000

    Ported and tested new monster motor control (need further test)

--- teensy/firmware/lib/motor/Motor.cpp
+++ teensy/firmware/lib/motor/Motor.cpp
@@ -5,7 +5,9 @@ Controller::Controller(driver motor_driver, int pwm_pin, int motor_pinA, int mot
     pwm_pin_(pwm_pin),
     motor_pinA_(motor_pinA),
     motor_pinB_(motor_pinB),
-	prev_pwm_(0)
+    prev_pwm_(0),
+    next_pwm_(0),
+    rampStartMillis(0)
 {
     switch (motor_driver)
     {
@@ -34,15 +36,15 @@ Controller::Controller(driver motor_driver, int pwm_pin, int motor_pinA, int mot
 
             //ensure that the motor is in neutral state during bootup
             motor_.writeMicroseconds(1500);
-
+            //TODO:  should I send the stop??
             break;
     }
 }
 
-int Controller::spin(int pwm)
+unsigned int  Controller::spin(int pwm)
 {
-	// return 0 if value is set, othwerwise a delay that has to be waited to re-set the value
-	int waitms = 0;
+    // return 1 if spin is sent, othwerwise a 0
+    int spin_possible = 1;
     switch (motor_driver_)
     {
         case L298:
@@ -57,6 +59,7 @@ int Controller::spin(int pwm)
                 digitalWrite(motor_pinB_, HIGH);
             }
             analogWrite(pwm_pin_, abs(pwm));
+            this->prev_pwm_ = pwm;
             break;
 
         case BTS7960:
@@ -75,20 +78,155 @@ int Controller::spin(int pwm)
                 analogWrite(motor_pinB_, 0);
                 analogWrite(motor_pinA_, 0);
             }
+            this->prev_pwm_ = pwm;
             break;
     
         case ESC:
-			// Check if the PWM must be reversed. Set to 0 as needed
-			if (pwm*this->prev_pwm_ < 0) { // sign change
-				waitms = abs(pwm-prev_pwm_) * NEUTRAL_BAND_WAIT_MS_PER_PWM; // TODO: Hardcoded for now, my undestanding is the difference in the pwm should stay in dead band of the ESC
-				pwm = 0;
-				motor_.writeMicroseconds(1500) ;
-			} else {
-				motor_.writeMicroseconds(1500 + pwm);
-			}
-			
-            break;
+          if (pwm==0 && this->prev_pwm_ != 0) { // RECEIVED A STOP, NON  IN STOP MODE, NEED TO MANAAGE A SLOW DOWN RAMP
+              motor_.writeMicroseconds(STOP_PWM_ESC); //Will send a 500us value to stop the ESC. accordingly with https://www.robotshop.com/community/blog/show/rc-speed-controller-esc-arduino-library
+              spin_possible = 0; // Setting flag spin is not possible
+              // start ramp!
+              if (this->rampStartMillis == 0) {
+#if BLDC_MTR_DEBUG==1
+sprintf(this->debug_interrupt_msg_ , "ESC: received PWM_0 %d with with ZC prev_pwm=%d,  starting ramp for PWM_0", pwm, this->prev_pwm_ );
+#endif 
+                this->next_pwm_ = 0;// this is also used to detect when a ramp starts
+                this->rampStartMillis = millis(); // Take start time for the ramp
+              } else {
+                  int32_t deltaMillis = millis() - this->rampStartMillis; // get the delta t from the ramp start
+                  int deltaPWM = this->next_pwm_ - this->prev_pwm_;
+                  int possiblePWM=0;
+
+                  // calculate the possible pwm, to verifify if the motor is halted
+                  if ( (deltaPWM > 0) ) {
+                     possiblePWM = round(deltaMillis * MAX_RAMP_PWM_PER_MS_POSITIVE_DELTA + this->prev_pwm_);
+                  }
+                  if (deltaPWM < 0) {
+                     possiblePWM = round(-deltaMillis * MAX_RAMP_PWM_PER_MS_NEGATIVE_DELTA + this->prev_pwm_);
+                  }
+                  if (possiblePWM*this->prev_pwm_ < 0) { // changed sign, Zero reached
+#if BLDC_MTR_DEBUG==1
+sprintf(this->debug_interrupt_msg_ , "ESC(%d): received PWM_0 possible pwm is %d with with ZC prev_pwm=%d,  ZERO REACHED, REMOVE RAMP  PWM_0", deltaMillis, possiblePWM, this->prev_pwm_ );
+#endif 
+                     this->next_pwm_ = 0;// this is also used to detect when a ramp starts
+                     this->prev_pwm_ = 0;
+                     this->rampStartMillis = 0;
+                  } else {
+#if BLDC_MTR_DEBUG==1
+sprintf(this->debug_interrupt_msg_ , "ESC(%d): received PWM_0 possible pwm is %d with with ZC prev_pwm=%d,  DO NOTHING  PWM_0", deltaMillis, possiblePWM, this->prev_pwm_ );
+#endif 
+                  }
+              }
+              break; // EXIT ESC CONTROL   
+          } 
+          
+          if (pwm==0 && this->prev_pwm_==0) { // AGAIN IN PWM
+              motor_.writeMicroseconds(STOP_PWM_ESC);
+              this->next_pwm_ = 0;// this is also used to detect when a ramp starts
+              this->prev_pwm_ = 0;
+              this->rampStartMillis = 0;
+#if BLDC_MTR_DEBUG==1
+sprintf(this->debug_interrupt_msg_ , "ESC: received PWM_0 REPETITION" );
+#endif             
+              break;
+          } 
+          
+          if  (pwm*this->prev_pwm_ >= 0) { // RECEIVED A CHANGE WITHOUT ZERO CROSSING OR RECOVERY FROM A ZERO
+#if BLDC_MTR_DEBUG==1
+             sprintf(this->debug_interrupt_msg_ , "ESC: received PWM %d with no ZC", pwm);
+#endif 
+              motor_.writeMicroseconds(1500 + pwm);
+              this->next_pwm_ = 0;// reset any eventual active ramp
+              this->prev_pwm_ = pwm;// set the actual ramp      
+              this->rampStartMillis = 0;      
+              break;  // EXIT ESC CONTROL
+          } 
+          if  (pwm*this->prev_pwm_ < 0) { // ZERO CROSSING DETECT, sign has changed
+              if (this->next_pwm_ == 0) { // It is the first time I received a ZC. Start the ramp
+     //-------------- ZERO CROSSING DETECT FIRST DETECT --------------
+                  motor_.writeMicroseconds(1500); // set idle
+                  spin_possible = 0; // Setting flag spin is not possible
+                  // start ramp!
+                  this->next_pwm_ = pwm;// this is also used to detect when a ramp starts
+                  this->rampStartMillis = millis(); // Take start time for the ramp
+#if BLDC_MTR_DEBUG==1
+                  sprintf(this->debug_interrupt_msg_ , "ESC: received PWM %d with with ZC prev_pwm=%d, first time, starting ramp ", pwm, this->prev_pwm_ );
+#endif 
+       // -------------- END  ZERO CROSSING DETECT FIRST DETECT --------------
+              } else { // Not the first time I got a zero crossing situation
+          //-------------- ZERO CROSSING DETECT FURTHER DETECT --------------
+                  //1. Calculate the possible PWM that accordingly the ramp could be applied
+                  int32_t deltaMillis = millis() - this->rampStartMillis; // get the delta t from the ramp start
+                  int deltaPWM = this->next_pwm_ - this->prev_pwm_;
+                  int possiblePWM=0;
+                  if (deltaPWM > 0) { // Ramp is positive
+                //-------------- ZERO CROSSING DETECT deltaPWM > 0 --------------
+                      possiblePWM = round(deltaMillis * MAX_RAMP_PWM_PER_MS_POSITIVE_DELTA + this->prev_pwm_);
+                      if (possiblePWM >= pwm) { // the requested pwm is accetable, 
+#if BLDC_MTR_DEBUG==1
+sprintf(this->debug_interrupt_msg_ , "ESC: POSITIVE deltaPWM(%d) - requested pwm vs possible pwm = %d , %d = round(deltaMillis(%d) * MAX_RAMP_PWM_PER_MS(%f) + this->prev_pwm_(%d) );  -> OK APPLY", deltaPWM, pwm, possiblePWM, deltaMillis, MAX_RAMP_PWM_PER_MS_POSITIVE_DELTA, this->prev_pwm_);
+#endif 
+                          motor_.writeMicroseconds(1500 + pwm);
+                          this->next_pwm_ = 0;// reset any eventual active ramp
+                          this->prev_pwm_ = pwm;// set the actual ramp            
+                          break;  // EXIT ESC CONTROL
+                      } else if (possiblePWM*this->prev_pwm_ < 0) { // what if the possible pwm cross the zero ? I can apply the reversed PWM
+#if BLDC_MTR_DEBUG==1
+sprintf(this->debug_interrupt_msg_ , "ESC: POSITIVE deltaPWM(%d) (ZC with possiblePWM) - requested pwm vs possible pwm = %d , %d = round(deltaMillis(%d) * MAX_RAMP_PWM_PER_MS(%f) + this->prev_pwm_(%d) );  ->  APPLY possiblePWM", deltaPWM, pwm, possiblePWM, deltaMillis, MAX_RAMP_PWM_PER_MS_POSITIVE_DELTA, this->prev_pwm_);
+#endif 
+                          motor_.writeMicroseconds(1500 + possiblePWM);// set the actual ramp // EXPERIMENTAL    
+                          this->prev_pwm_ = possiblePWM;// set the actual ramp // EXPERIMENTAL   
+                          this->next_pwm_ = 0;// reset any eventual active ramp
+                          this->rampStartMillis = 0;
+                          break;  // EXIT ESC CONTROL
+                       } else {
+#if BLDC_MTR_DEBUG==1
+sprintf(this->debug_interrupt_msg_ , "ESC: POSITIVE deltaPWM(%d) - requested pwm vs possible pwm = %d , %d = round(deltaMillis(%d) * MAX_RAMP_PWM_PER_MS(%f) + this->prev_pwm_(%d) );  ->  NOT APPLY", deltaPWM, pwm, possiblePWM, deltaMillis, MAX_RAMP_PWM_PER_MS_POSITIVE_DELTA, this->prev_pwm_);
+#endif 
+                          spin_possible = 0; // Setting flag spin is not possible
+                          break;  // EXIT ESC CONTROL
+                      }  
+               //-------------- END ZERO CROSSING DETECT deltaPWM > 0 --------------
+                  } else { // Ramp is negative
+               //-------------- ZERO CROSSING DETECT deltaPWM < 0 --------------
+                      // I am assuming  deltaPWM==0 is never possible in this part of the code!
+                      // if (deltaPWM==0) should be checked, NOTE: during test i didnt notice any problem. Probably is assured by the design :) 
+                      possiblePWM = round(-deltaMillis * MAX_RAMP_PWM_PER_MS_NEGATIVE_DELTA + this->prev_pwm_);
+                      if (possiblePWM <= pwm) { // the requested pwm is accetable,  
+#if BLDC_MTR_DEBUG==1
+sprintf(this->debug_interrupt_msg_ , "ESC: NEGATIVE deltaPWM(%d) - requested pwm vs possible pwm = %d , %d = round(-deltaMillis(%d) * MAX_RAMP_PWM_PER_MS(%f) + this->prev_pwm_(%d) );  ->  OK APPLY", deltaPWM, pwm, possiblePWM, deltaMillis, MAX_RAMP_PWM_PER_MS_NEGATIVE_DELTA, this->prev_pwm_);
+#endif 
+                          motor_.writeMicroseconds(1500 + pwm);
+                          this->next_pwm_ = 0;// reset any eventual active ramp
+                          this->prev_pwm_ = pwm;// set the actual ramp      
+                          this->rampStartMillis = 0;      
+                          break;  // EXIT ESC CONTROL
+                      } else if (possiblePWM*this->prev_pwm_ < 0) { // what if the possible pwm cross the zero ? I can apply the reversed PWM
+#if BLDC_MTR_DEBUG==1
+sprintf(this->debug_interrupt_msg_ , "ESC: NEGATIVE deltaPWM(%d) - requested pwm vs possible pwm = %d , %d = round(-deltaMillis(%d) * MAX_RAMP_PWM_PER_MS(%f) + this->prev_pwm_(%d) );  ->  APPLY possiblePWM", deltaPWM, pwm, possiblePWM, deltaMillis, MAX_RAMP_PWM_PER_MS_NEGATIVE_DELTA, this->prev_pwm_);
+#endif 
+                          motor_.writeMicroseconds(1500 + pwm);// set the actual ramp // EXPERIMENTAL    
+                          this->prev_pwm_ = pwm;// set the actual ramp // EXPERIMENTAL   
+                      } else {
+        
+#if BLDC_MTR_DEBUG==1
+sprintf(this->debug_interrupt_msg_ , "ESC: NEGATIVE deltaPWM(%d) - requested pwm vs possible pwm = %d , %d = round(-deltaMillis(%d) * MAX_RAMP_PWM_PER_MS(%f) + this->prev_pwm_(%d) );  ->  NOT APPLY", deltaPWM, pwm, possiblePWM, deltaMillis, MAX_RAMP_PWM_PER_MS_NEGATIVE_DELTA, this->prev_pwm_);
+#endif 
+                          spin_possible = 0; // Setting flag spin is not possible   
+                          break;  // EXIT ESC CONTROL                  
+                      }
+               //-------------- ZERO CROSSING DETECT deltaPWM < 0 --------------
+                  } 
+                }            
+          }
+          break;
     }
-	this->prev_pwm_ = pwm;
-	return waitms;
+  #if BLDC_MTR_DEBUG==1
+    Serial.println("#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*"); 
+    Serial.print("prev pwm= "); Serial.print(this->prev_pwm_); Serial.print(" requested pwm "); Serial.print(pwm);  Serial.print(" next pwm "); Serial.println(this->next_pwm_);
+    Serial.print("msg ");Serial.println(this->debug_interrupt_msg_);
+    Serial.println("#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*");  
+    
+  #endif 
+  return spin_possible;
 }
--- teensy/firmware/lib/motor/Motor.h
+++ teensy/firmware/lib/motor/Motor.h
@@ -4,17 +4,27 @@
 #include <Servo.h> 
 #include <Arduino.h>
 
-#ifndef NEUTRAL_BAND_WAIT_MS_PER_PWM
-#define NEUTRAL_BAND_WAIT_MS_PER_PWM 3
+
+#ifndef MAX_RAMP_PWM_PER_MS_POSITIVE_DELTA // Max steep for a ramp when a zero crossing happen.  The ramp is positive (from a negative PWM to a positive PWM)
+                             // The PWM has set to 0 and wait before apply the new PWM, accordingly to this max steep ramp
+#define MAX_RAMP_PWM_PER_MS_POSITIVE_DELTA 0.02 //  Max velocity of INCREASE a PWM per msec, (NOTE:  BREAK SEEMS APPLIED BY THE ESC)
+                                                //0.04 seems the max velocity, but for consitency it is better to set equal in both cases
 #endif
+
+#ifndef MAX_RAMP_PWM_PER_MS_NEGATIVE_DELTA // Max steep for a ramp when a zero crossing happen. The ramp is negative (from a positive PWM to a negative PWM)
+#define MAX_RAMP_PWM_PER_MS_NEGATIVE_DELTA 0.02 // Max velocity of INCREASE a PWM per msec, (NOTE: NO BREAK APPLIED BY THE ESC??)
+#endif
+
+//#define BLDC_MTR_DEBUG 1 // it deosnt work if set in the main. mah
+#define STOP_PWM_ESC 1500 // which is the stop pwm?
 
 class Controller
 {
     public:
         enum driver {L298, BTS7960, ESC};
         Controller(driver motor_driver, int pwm_pin, int motor_pinA, int motor_pinB);
-        int spin(int pwm);
-		int latest_spin_applied() { return this->prev_pwm_;};
+        unsigned int  spin(int pwm);
+        int latest_spin_applied() { return this->prev_pwm_;};
 
     private:
         Servo motor_;
@@ -22,7 +32,13 @@ class Controller
         int pwm_pin_;
         int motor_pinA_;
         int motor_pinB_;
-		int prev_pwm_;
+        int prev_pwm_;
+        int next_pwm_;                  // USED ONLY FOR ESC
+        unsigned long rampStartMillis;  // USED ONLY FOR ESC
+#if BLDC_MTR_DEBUG==1
+        char debug_interrupt_msg_[400];
+#endif 
+        
 };
 
 #endif
